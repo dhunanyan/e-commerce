485288cb619cbb2fac388f59ee2382609222fd29
dhunanyan <davit.hunanyan@me.com>
2022-02-06 04:56:55 +0100

    sign-in handleSubmit

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -26,6 +26,7 @@ class SignUp extends React.Component {
 
     if (password !== confirmPassword) {
       alert("Passwords don't match!");
+      this.setState({ password: "", confirmPassword: "" });
       return;
     }
 
