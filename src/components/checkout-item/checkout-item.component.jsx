1880bef8f3e078153e4e38e69cf606af4c33f896
dhunanyan <davit.hunanyan@me.com>
2022-02-15 21:07:58 +0100

    remove from card checkout component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
 import { MdKeyboardArrowLeft as LeftArrow } from "react-icons/md";
 import { MdKeyboardArrowRight as RighttArrow } from "react-icons/md";
@@ -11,7 +15,14 @@ import { CSSTransition } from "react-transition-group";
 import "./checkout-item.styles.scss";
 import { animateScroll as scroll } from "react-scroll";
 
-const CheckoutItem = ({ cartItem, clearItem, onClick, index }) => {
+const CheckoutItem = ({
+  cartItem,
+  clearItem,
+  addItem,
+  removeItem,
+  onClick,
+  index,
+}) => {
   const { name, quantity, price, imageUrl } = cartItem;
   const [isRemoved, setIsRemoved] = useState(false);
 
@@ -23,14 +34,40 @@ const CheckoutItem = ({ cartItem, clearItem, onClick, index }) => {
       classNames="fade-out"
     >
       <div className="checkout-item">
-        <div className="image-container">
+        <div className="image-container checkout-item__inner">
           <img src={imageUrl} alt="Checkout-Item" />
         </div>
-        <span className="name">{name}</span>
-        <span className="quantity">{quantity}</span>
-        <span className="price">{price}</span>
+        <div className="name checkout-item__inner">{name}</div>
+        <div className="quantity checkout-item__inner">
+          <div
+            className="arrow"
+            onClick={() => {
+              removeItem(cartItem);
+              console.log(quantity);
+
+              if (quantity === 1) {
+                setIsRemoved(true);
+                setTimeout(() => setIsRemoved(false), 250);
+                setTimeout(() => clearItem(cartItem), 1000);
+                onClick(index);
+              }
+            }}
+          >
+            <LeftArrow />
+          </div>
+          <span className="value">{quantity}</span>
+          <div
+            className="arrow"
+            onClick={() => {
+              addItem(cartItem);
+            }}
+          >
+            <RighttArrow />
+          </div>
+        </div>
+        <div className="price checkout-item__inner">{price}</div>
         <div
-          className="remove-button"
+          className="remove-button checkout-item__inner"
           onClick={() => {
             setIsRemoved(true);
             setTimeout(() => setIsRemoved(false), 250);
@@ -47,6 +84,8 @@ const CheckoutItem = ({ cartItem, clearItem, onClick, index }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
